04a6db4e6f82b44669d87364f1c2079efae3480e
silvanoamaral <silvano.amaral9@gmail.com>
2024-05-06 20:55:38 -0300

    adding a router error

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import App from "./App"
 import { store } from "./app/store"
 import "./index.css"
+import ErrorPage from "./page"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+])
 
 const container = document.getElementById("root")
 
@@ -13,7 +24,7 @@ if (container) {
   root.render(
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </React.StrictMode>,
   )
